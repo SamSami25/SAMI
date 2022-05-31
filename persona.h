ab92b88d838ade665b8b6e7acaf49194640db699
Sami Aldaz <saldazc@est.ups.edu.ec>
2022-05-31 10:34:28 -0500

    Creada la clase Persona

---
+++ persona.h
@@ -0,0 +1,18 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Persona
+{
+public:
+
+    string nombre;
+    int edad;
+    Persona();
+};
+
+#endif // PERSONA_H
